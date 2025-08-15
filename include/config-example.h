289abeb29aee5c3ef244f485db7911b48e4d2b66
hump-coder <karl.bridger@gmail.com>
2025-08-15 11:05:03 +0930

    Expose receiver WiFi and reboot commands as HA buttons

--- include/config-example.h
+++ include/config-example.h
@@ -8,6 +8,9 @@
 #define WIFI_SSID "your ssid"
 #define WIFI_PASS "your wifi pass"
 
+#define WIFI_CUSTOM_SSID "custom wifi ssid"
+#define WIFI_CUSTOM_PASS "custom wifi pass"
+
 #define MQTT_SERVER "192.168.1.11"
 #define MQTT_PORT "1883"
 #define MQTT_USER "mqtt_user"
